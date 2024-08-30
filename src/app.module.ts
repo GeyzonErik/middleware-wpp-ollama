8fbef70cb21a0a814d3a6c817d01719e9073d1f2
GeyzonErik <erikles.geyzon@gmail.com>
2024-08-30 00:40:51 -0300

    feature: importing SharedModules in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WhatsappModule } from './whatsapp/domain/whatsapp.module';
+import { WhatsappModule } from './whatsapp/whatsapp.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [WhatsappModule],
+  imports: [WhatsappModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
