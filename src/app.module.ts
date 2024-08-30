dd8b4db70c4112c2d99fff9a496b7b8e034f659c
GeyzonErik <erikles.geyzon@gmail.com>
2024-08-30 00:43:35 -0300

    refactor: removing AppService and AppControler from app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
 import { SharedModule } from './shared/shared.module';
 
 @Module({
   imports: [WhatsappModule, SharedModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
