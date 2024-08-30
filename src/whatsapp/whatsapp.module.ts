e58c508b199f17baf8dd571b0a87bb10eb4895df
GeyzonErik <erikles.geyzon@gmail.com>
2024-08-30 00:40:11 -0300

    refactor: changing whatsapp.module folder

---
+++ src/whatsapp/whatsapp.module.ts
@@ -0,0 +1,56 @@
+import { Module } from '@nestjs/common';
+import { WhatsappController } from './presentation/controller/whatsapp.controller';
+import { WhatsappService } from './application/service/whatsapp.service';
+import { HttpAdapter } from 'src/shared/infra/http/http-adapter.service';
+import { ConnectToWhatsapp } from './application/usecases/connect-to-whatsapp.usecase';
+import { OnMessage } from './application/usecases/on-message.usecase';
+import { SendMessage } from './application/usecases/send-message.usecase';
+import { ChatAddapter } from '../shared/gateways/chats/chat.adapter';
+import { ChatModule } from 'src/shared/gateways/chats/chat.module';
+import { SharedModule } from 'src/shared/shared.module';
+
+@Module({
+  imports: [SharedModule, ChatModule],
+  providers: [
+    // Services
+    {
+      provide: WhatsappService,
+      useFactory: (
+        HttpAdapter: HttpAdapter,
+        connectToWhatsapp: ConnectToWhatsapp,
+        onMessage: OnMessage,
+        sendMessage: SendMessage,
+      ) => {
+        return new WhatsappService(
+          HttpAdapter,
+          connectToWhatsapp,
+          onMessage,
+          sendMessage,
+        );
+      },
+      inject: [HttpAdapter, ConnectToWhatsapp, OnMessage, SendMessage],
+    },
+
+    // UseCases
+    {
+      provide: ConnectToWhatsapp,
+      useFactory: (whatsappGateway: ChatAddapter) =>
+        new ConnectToWhatsapp(whatsappGateway),
+      inject: [ChatAddapter],
+    },
+    {
+      provide: OnMessage,
+      useFactory: (whatsappGateway: ChatAddapter) =>
+        new OnMessage(whatsappGateway),
+      inject: [ChatAddapter],
+    },
+    {
+      provide: SendMessage,
+      useFactory: (whatsappGateway: ChatAddapter) =>
+        new SendMessage(whatsappGateway),
+      inject: [ChatAddapter],
+    },
+  ],
+  controllers: [WhatsappController],
+})
+export class WhatsappModule {}
